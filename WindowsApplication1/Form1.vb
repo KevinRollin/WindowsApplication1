ae40f3853634659137753e52bb144cb3dbff69c3
KevinRollin <kevinrollin@gmail.com>
2014-08-22 14:13:17 -0400

    test ultime

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        fuuuuuuckkkkk youuuu alllll
+        fuuuuuuckkkkk youuuu allllll
         kfjf(pppp)
     End Sub
 End Class
